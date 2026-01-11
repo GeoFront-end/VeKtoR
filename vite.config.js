41b8310532e7f373487d78c62c6d33c9e4ed9ab5
Gheorghe Rapesco <gheorgherapesco@gmail.com>
2026-01-11 12:54:36 +0200

    vite fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/VeKtoR/",
+  root: '.', // root directory
+  build: {
+    outDir: 'dist', // output folder
+    rollupOptions: {
+      input: './index.html', // main entry
+    },
+  },
 });
